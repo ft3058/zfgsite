59a30b1274d457ae88458e07a6e3e053c25b219e
duguanglin <twotiger@localhost.localdomain>
2016-04-13 04:06:01 -0400

    ‘add something

--- connect.py
+++ connect.py
@@ -310,21 +310,6 @@ class Tty(object):
         try:
             # role_key = connect_info.get('role_key')
             asset = Asset.objects.get(ip=connect_info.get('ip'))
-            # if role_key and os.path.isfile(role_key):
-            try:
-                ssh.connect(connect_info.get('ip'),
-                            port = asset.port,
-                            username = asset.username,
-                            password = asset.passwd,
-                            # username=connect_info.get('role_name'),
-                            # password=connect_info.get('role_pass'),
-                            # key_filename=role_key,
-                            # look_for_keys=False
-                            )
-                return ssh
-            except (paramiko.ssh_exception.AuthenticationException, paramiko.ssh_exception.SSHException):
-                logger.warning(u'使用ssh key 失败, 尝试只使用密码')
-                pass
 
             ssh.connect(connect_info.get('ip'),
                         port=asset.port,
@@ -332,8 +317,9 @@ class Tty(object):
                         password = asset.passwd,
                         # username=connect_info.get('role_name'),
                         # password=connect_info.get('role_pass'),
-                        allow_agent=False,
-                        look_for_keys=False)
+                        # allow_agent=False,
+                        # look_for_keys=False
+            )
 
         except paramiko.ssh_exception.AuthenticationException, paramiko.ssh_exception.SSHException:
             tag, res = 'fail', '认证失败 (可能错误:密码错误)'
